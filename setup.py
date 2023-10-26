d859c3c857d5905ffa0656f4d8112ffc84b6cd3c
Mohammad Farooq <mbf4ay@virgini.edu>
2023-10-25 22:57:32 -0400

    change

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'Booklover code',
+    version = '0.1',
+    author = 'Mohammad Farooq',
+    author_email = 'mbf4ay@virginia.edu',
+    packages = ['booklover_code'],
+    url = 'https://github.com/Mofarooq1/mbf4ay_Module_9_HW',
+    license = 'LICENSE.txt',
+    description = 'A package that is used for HW09',
+)
